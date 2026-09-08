d9ef1afc0fd5455a4c495958ad302f5d25bd78d1
jeily baudino <jeilybaudino.itsu@gmail.com>
2026-09-07 21:35:20 -0400

    error de la calse 2

--- activities/class-02/project/src/server.js
+++
@@ -1,8 +0,0 @@
-// Entry point: this file only starts the process. It does not know about routes.
-import app from './app.js';
-
-const PORT = 3000;
-
-app.listen(PORT, () => {
-  console.log(`Request API Full is running on http://localhost:${PORT}`);
-});
